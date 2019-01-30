2fff3c8ec8ba14309fe3e680b717ac764e68fa25
Bambusoideae <46619411+Bambusaceae@users.noreply.github.com>
2019-01-30 19:07:40 +0800

    增加赋值、正负运算符。

--- Bignum/declaration/bignum.h
+++ Bignum/declaration/bignum.h
@@ -12,19 +12,27 @@ namespace std{
 			vector<short> NUM;
 			bool MINUS;
 			
+			inline void clear();
 			inline void deleteLeaderZero();
 		
 		public:
 			_BIGNUM();
 			~_BIGNUM();
 			
-			inline void clear();
+			inline _BIGNUM& operator = (_BIGNUM);
+			inline _BIGNUM& operator = (bool);
+			inline _BIGNUM& operator = (char);
+			inline _BIGNUM& operator = (short);
+			inline _BIGNUM& operator = (unsigned short);
+			inline _BIGNUM& operator = (int);
+			inline _BIGNUM& operator = (unsigned int);
+			inline _BIGNUM& operator = (long);
+			inline _BIGNUM& operator = (unsigned long);
+			inline _BIGNUM& operator = (long long);
+			inline _BIGNUM& operator = (unsigned long long);
 			
-			inline _BIGNUM operator = (_BIGNUM);
-			inline _BIGNUM operator = (char);
-			inline _BIGNUM operator = (int);
-			inline _BIGNUM operator = (long);
-			inline _BIGNUM operator = (long long);
+			inline const _BIGNUM& operator + ();
+			inline const _BIGNUM operator - ();
 			
 			friend ostream& operator << (ostream&,const _BIGNUM&);
 			friend istream& operator >> (istream&,_BIGNUM&);
