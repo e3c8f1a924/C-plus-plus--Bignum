59c1af4cda124a2144f62d515da1a6dc7517edcc
Bambusoideae <46619411+Bambusaceae@users.noreply.github.com>
2019-01-30 19:06:21 +0800

    增加赋值、正负运算符。

--- Bignum/define/bignum.h
+++ Bignum/define/bignum.h
@@ -4,11 +4,12 @@
 namespace std{
 	_BIGNUM::_BIGNUM(){
 		this->NUM.push_back(0);
+		this->MINUS=0;
 	}
 	_BIGNUM::~_BIGNUM(){
 		
 	}
-	
+	 
 	void _BIGNUM::deleteLeaderZero(){
 		for(vector<short>::iterator i=this->NUM.end()-1;i!=this->NUM.begin();i--){
 			if(*i==0){
@@ -21,15 +22,164 @@ namespace std{
 	
 	void _BIGNUM::clear(){
 		vector<short>().swap(this->NUM);
+		this->MINUS=0;
+	}
+	//Operator =
+	inline _BIGNUM& _BIGNUM::operator = (_BIGNUM __n){
+		this->MINUS = __n.MINUS;
+		vector<short> __n_T(__n.NUM);
+		this->NUM.swap(__n_T);
+		return *this;
 	}
 	
-	ostream& operator << (ostream& OS,const _BIGNUM& __n){
-		ostringstream oss;
-		string NUM_STR="";
-		for(vector<short>::const_iterator i=__n.NUM.begin();i!=__n.NUM.end();i++){
-			oss<<*i;
+	inline _BIGNUM& _BIGNUM::operator = (bool __n){
+		this->operator = ((int)__n);
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (char __n){
+		this->operator = ((int)__n);
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (short __n){
+		short __n_T=__n;
+		this->clear();
+		if(__n_T<0){
+			this->MINUS=1;
+			__n_T=-__n_T;
 		}
-		NUM_STR=oss.str();
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (unsigned short __n){
+		unsigned short __n_T=__n;
+		this->clear();
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (int __n){
+		int __n_T=__n;
+		this->clear();
+		if(__n_T<0){
+			this->MINUS=1;
+			__n_T=-__n_T;
+		}
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (unsigned int __n){
+		unsigned int __n_T=__n;
+		this->clear();
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (long __n){
+		long __n_T=__n;
+		this->clear();
+		if(__n_T<0){
+			this->MINUS=1;
+			__n_T=-__n_T;
+		}
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (unsigned long __n){
+		unsigned long __n_T=__n;
+		this->clear();
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (long long __n){
+		long long __n_T=__n;
+		this->clear();
+		if(__n_T<0){
+			this->MINUS=1;
+			__n_T=-__n_T;
+		}
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	
+	inline _BIGNUM& _BIGNUM::operator = (unsigned long long __n){
+		unsigned long long __n_T=__n;
+		this->clear();
+		if(__n_T==0){
+			this->NUM.push_back(0);
+		}
+		while(__n_T>0){
+			this->NUM.push_back(__n_T%10);
+			__n_T/=10;
+		}
+		return *this;
+	}
+	//Operator +
+	inline const _BIGNUM& _BIGNUM::operator + (){
+		return *this;
+	}
+	//Operator -
+	inline const _BIGNUM _BIGNUM::operator - (){
+		_BIGNUM __n_T=(*this);
+		__n_T.MINUS=!__n_T.MINUS;
+		return __n_T;
+	}
+	//Standard Input/Output
+	ostream& operator << (ostream& OS,const _BIGNUM& __n){
+		ostringstream OSS;
+		string NUM_STR="";
+		if(__n.MINUS==1)OS<<'-';
+		for(vector<short>::const_iterator i=__n.NUM.begin();i!=__n.NUM.end();i++){
+			OSS<<*i;
+		}
+		NUM_STR=OSS.str();
 		for(int i=NUM_STR.length()-1;i>=0;i--){
 			OS<<NUM_STR[i];
 		}
